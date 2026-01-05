7f71b5c67423ceb8535b3a773b8404e5537b3610
Leandro Magallanes <patagpniascript@gmail.com>
2026-01-05 08:20:16 -0300

    delete lib messages

--- src/hooks/getMessagesText.js
+++
@@ -1,6 +0,0 @@
-export const getMessagesText = (comments) => {
-  const count = comments?.length || 0;
-  if (count === 0) return "aún no tiene mensajes, escribe uno 💌";
-  if (count === 1) return "ver 1 mensaje";
-  return `ver ${count} mensajes`;
-};
